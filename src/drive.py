a166283444143255e7e06ba094fe7d587a0ed014
Érick de Oliveira Teixeira <38773400+ErickOliveiraT@users.noreply.github.com>
2022-07-02 12:23:20 -0300

    list drive subfolders

--- src/drive.py
+++ src/drive.py
@@ -1,40 +1,27 @@
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from tinydb import TinyDB
-import auth
-import sys
 
-def main():
+from googleapiclient.errors import HttpError
+
+def list_files(parent_id, drive_service, drive_files, parents=None):
     try:
-        db = TinyDB('drive_files.json')
-        creds = auth.get_credentials()
-        if not creds:
-            print('Error while retrieving credentials')
-            sys.exit(0)
-        service = build('drive', 'v3', credentials=creds)
         page_token = None
         while True:
-            response = service.files().list(q="'{}' in parents".format('1rkSVumNLsAuD-F-anTHGzBly2D43Qyye'),
+            response = drive_service.files().list(q="'{}' in parents".format(parent_id),
                 spaces='drive',
                 fields='nextPageToken, files(id, name, parents, md5Checksum, fileExtension, size, createdTime, modifiedTime)',
                 pageToken=page_token).execute()
             for file in response.get('files', []):
-                file["type"] = 'file'
                 if not "fileExtension" in file.keys():
                     file["type"] = 'folder'
-                print(file)
-                db.insert(file)
+                else:
+                    file["type"] = 'file'
+                if parents:
+                    file["parents"] = parents + file["parents"]
+                print(f'Checking {file["name"]}')
+                drive_files.insert(file)
+                if file["type"] == 'folder':
+                    list_files(file["id"], drive_service, drive_files, file["parents"])
             page_token = response.get('nextPageToken', None)
             if page_token is None:
                 break
     except HttpError as error:
         print(f'An error occurred: {error}')
-
-def get_file(file_id, creds):
-    if not creds:
-        print('Must provide creds')
-        return None
-    service = build('drive', 'v3', credentials=creds)
-
-if __name__ == '__main__':
-    main()
---
+++ src/main.py
@@ -0,0 +1,32 @@
+from googleapiclient.discovery import build
+from tinydb import TinyDB, where
+import drive
+import auth
+import sys
+
+remote_dir = '1l-sKq0MPteFHFQO_iDUYqLfCe-OcY31n'
+
+def main():
+    #remote drive files database
+    drive_files = TinyDB('drive_files.json')
+    drive_files.remove(where('id') > '')
+
+    #start google drive service
+    creds = auth.get_credentials()
+    if not creds:
+        print('Error while retrieving credentials')
+        sys.exit(0)
+    service = build('drive', 'v3', credentials=creds)
+
+    #list remote drive files
+    drive.list_files(remote_dir, service, drive_files)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
