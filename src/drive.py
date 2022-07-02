50bfbe6888ad52ff42690407012cf05b00f36d7c
Érick de Oliveira Teixeira <38773400+ErickOliveiraT@users.noreply.github.com>
2022-07-02 13:01:21 -0300

    path builder cache

--- src/drive.py
+++ src/drive.py
@@ -30,13 +30,18 @@ def list_files(parent_id, drive_service, drive_files, parents=None):
 def build_paths(drive_files):
     files = drive_files.all()
     File = Query()
+    cache = {}
     for file in files:
         if len(file["parents"]) == 1:
             file["path"] = './' + file["name"]
         else:
             path = './'
             for i in range(1,len(file["parents"])):
-                match = drive_files.search(File.id == file["parents"][i])
-                path += match[0]["name"] + '/'
+                if file["parents"][i] in cache.keys():
+                    path += cache[file["parents"][i]] + '/'
+                else:
+                    match = drive_files.search(File.id == file["parents"][i])
+                    path += match[0]["name"] + '/'
+                    cache[file["parents"][i]] = match[0]["name"]
             file["path"] = path + file["name"]
         drive_files.update(file, File.id == file['id'])
--- src/main.py
+++ src/main.py
@@ -9,9 +9,11 @@ remote_dir = '1l-sKq0MPteFHFQO_iDUYqLfCe-OcY31n'
 def main():
     #remote drive files database
     drive_files = TinyDB('drive_files.json')
+    print('Cleaning database')
     drive_files.remove(where('id') > '')
 
     #start google drive service
+    print('Starting Google Drive Service')
     creds = auth.get_credentials()
     if not creds:
         print('Error while retrieving credentials')
@@ -19,9 +21,11 @@ def main():
     service = build('drive', 'v3', credentials=creds)
 
     #list remote drive files
+    print('Checking remote files')
     drive.list_files(remote_dir, service, drive_files)
 
     #build path for drive files
+    print('Building paths for remote files')
     drive.build_paths(drive_files)
 
 
